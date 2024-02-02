8eb692372a2d0c371cc8c08a1f218b85d440e31e
Mingi Kang <gi3592@gmail.com>
2024-02-02 16:57:23 +0900

    #8
    학점계산기 추가

--- src/main/java/ssg/com/a/controller/MypageController.java
+++ src/main/java/ssg/com/a/controller/MypageController.java
@@ -1,16 +1,34 @@
 package ssg.com.a.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import ssg.com.a.dto.MygradecalDto;
 
 
 @Controller
 public class MypageController {
 
+	// TODO 페이지 이동란
+	// 마이페이지 메인화면 이동
 	@GetMapping("mymain.do")
 	public String mymain() {
+		System.out.println("MypageController mymain" + new Date());
 		return "mypage/mymain";
 	}
+	
+	// 학점계산기 페이지 이동
+	@GetMapping("mygradecal.do")
+	public String mygradecal() {
+		System.out.println("MypageController mygradecal" + new Date());
+		return "mypage/mygradecal";
+	}
+	
 	
 }
