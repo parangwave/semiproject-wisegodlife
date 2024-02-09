322319b5fe52837d8f8ca838eafb66a731247448
jisu <parangwavetraveller@gmail.com>
2024-02-09 22:36:13 +0900

    #44 feat: clock.js에 날짜, 요일 추가
    getDateStr() 함수를 통해 날짜, 요일을 실시간 변동하도록 함

--- src/main/webapp/js/mainPage/clock.js
+++ src/main/webapp/js/mainPage/clock.js
@@ -1,6 +1,8 @@
 
 const clockContainer = document.querySelector("#mainContainer__leftTab__weather__clockSection"),
- clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1"),
+    CalContainer = document.querySelector("#mainContainer__leftTab__weather__clockSection"),
+    CalTitle = CalContainer.querySelector("h4");
 
 function getTime(){
     const date = new Date();
@@ -13,12 +15,30 @@ function getTime(){
     }`;
     // :${seconds < 10 ? `0${seconds}` : seconds
     // }`;
+
     return hours;
+}
+
+function getDateStr(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const aDate = date.getDate();
+    const day = date.getDay();
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+    console.log(year + " " + month + " " + aDate + " " + day);
+    
+    CalTitle.innerText = `${year}년 ${
+        `${month}`}월 ${
+        `${aDate}`}일 ${
+            week[day]}요일
+    `;
 }
 
 function init() {
     getTime();
     setInterval(getTime, 1000);
+    getDateStr();
 }
 
 init();
