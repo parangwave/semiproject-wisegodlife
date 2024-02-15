e84bff0e7c4ab5d3612d0cc68977515331221bf1
jisu <parangwavetraveller@gmail.com>
2024-02-15 15:14:18 +0900

    #68 feat: FindMateController의 FindMateController(Model model) 수정
    메이트 구하기 게시판의 글 목록을 가져오지 못하는 문제가 생김. 그래서 전역변수로 List<FindMateDto> list를 초기화함.
    list가 null인 경우에는 service에서 list를 들고 오게 만듦.
    그렇게 확인 작업을 거친 후 model에 list를 추가해주고 완료용 출력문을 추가함.

--- src/main/java/ssg/com/a/controller/FindMateController.java
+++ src/main/java/ssg/com/a/controller/FindMateController.java
@@ -22,13 +22,25 @@ public class FindMateController {
 	
 	@Autowired
 	HttpServletRequest request;
+	
+	List<FindMateDto> list = null;
 
 	@GetMapping("findmate.do")
 	public String FindMateList(Model model) {
 		System.out.println("FindMateController FindMateList " + new Date());
-		List<FindMateDto> list = null;
+//		List<FindMateDto> list = null;	// 잘못된 방식
 
-		model.addAttribute("list", list);
+		// 하나의 예시
+		System.out.println(list);	
+		if (list == null) {
+			// 하고자하는 프로세스 
+			this.list = service.findMateList();
+		}
+		System.out.println("list가 null이 아님");
+		
+		// 리턴값 주기
+		model.addAttribute("list", this.list);
+		System.out.println("된다");
 
 		return "findmate/findmate";
 	}
