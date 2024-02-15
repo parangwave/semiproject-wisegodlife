f11e316c9d700cb60f9bb0de654079d787798569
jisu <parangwavetraveller@gmail.com>
2024-02-15 15:19:41 +0900

    #68 feat: findMateController의 commentWriteAf(FindMateDto bc)를 수정
    HttpSession을 이용하여 login한 유저의 정보를 들고옴.
    메소드 파라미터 bc에서 nickname, profileImg지 값이 없었기 때문에 session을 통해서 해당 정보를 들고옴.
    그리고 setter를 통해서 데이터들을 bc에 넣어주고 댓글dto를 null 에러 없이 생성하도록 함.

--- src/main/java/ssg/com/a/controller/FindMateController.java
+++ src/main/java/ssg/com/a/controller/FindMateController.java
@@ -4,11 +4,13 @@ import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ssg.com.a.dto.FindMateDto;
 import ssg.com.a.dto.FriendDto;
@@ -16,12 +18,15 @@ import ssg.com.a.service.FindMateService;
 
 @Controller
 public class FindMateController {
-
+	
 	@Autowired
 	FindMateService service;
 	
 	@Autowired
 	HttpServletRequest request;
+	
+	@Autowired
+	HttpSession session;
 	
 	List<FindMateDto> list = null;
 
@@ -43,5 +48,51 @@ public class FindMateController {
 		System.out.println("된다");
 
 		return "findmate/findmate";
+	}
+	
+//	@ResponseBody
+//	@GetMapping("findmatelist.do")
+//	public List<FindMateDto> findMateList(int seq){
+//		System.out.println("findMateList commentList " + new Date());
+//		
+//		List<FindMateDto> list = service.findMateList();
+//		return list;
+//	}
+//	@GetMapping("findmatelist.do")
+//	public String findMateList(Model model){
+//		System.out.println("findMateList commentList " + new Date());
+//		
+//		List<FindMateDto> list = service.findMateList();
+//		model.addAttribute("list", list);
+//		
+//		return "findmate/findmate";
+//	}
+	
+	@PostMapping("findMatecommentwrite.do")
+	public String commentWriteAf(FindMateDto bc) {
+		System.out.println("commentWriteAf CommentWriteAf " + new Date());
+		
+		// 작성된 값 집어넣기
+		System.out.println(bc.toString());
+		// 변수값 정의
+		// String id; / String id = null; || int seq; / int seq = 0; / int seq = null(X)
+		
+		// table에 bc 넣어주기
+		FriendDto login = (FriendDto) session.getAttribute("login");
+//		System.out.println(login.toString());
+		
+		String _profileImg = login.getChangeprofile();
+		String _nickname = login.getNickname();
+//		System.out.println("_profileImg: " + _profileImg);
+//		System.out.println("_nickname: " + _nickname);
+		bc.setProfileImg(_profileImg);
+		bc.setNickname(_nickname);
+//		System.out.println(bc.toString());
+		
+		service.writeFindMate(bc);
+		
+		this.list.add(bc);
+		
+		return "redirect:/findmate.do";
 	}
 }
